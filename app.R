8cb79171d2b5b39844cd8be3353d2093c73c398e
Anthony Sena <asena5@its.jnj.com>
2025-06-28 23:20:15 -0400

    Adding app

---
+++ app.R
@@ -0,0 +1,40 @@
+library(OhdsiShinyModules)
+library(ShinyAppBuilder)
+
+shinyConfig <- initializeModuleConfig() |>
+  addModuleConfig(
+    createDefaultAboutConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultDatasourcesConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultCohortGeneratorConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultCohortDiagnosticsConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultCharacterizationConfig()
+  ) 
+
+
+cli::cli_h1("Starting shiny server")
+serverStr <- paste0(Sys.getenv("shinydbServer"), "/", Sys.getenv("shinydbDatabase"))
+cli::cli_alert_info("Connecting to {serverStr}")
+connectionDetails <- DatabaseConnector::createConnectionDetails(
+  dbms = "postgresql",
+  server = serverStr,
+  port = Sys.getenv("shinydbPort"),
+  user = "shinyproxy",
+  password = Sys.getenv("shinydbPw")
+)
+
+cli::cli_h2("Loading schema")
+createShinyApp(
+   config = shinyConfig,
+   connectionDetails = connectionDetails,
+   resultDatabaseSettings = createDefaultResultDatabaseSettings(schema = "rwecuimc"),
+   title = "RWE CUIMC App",
+   studyDescription = "RWE CUIMC App"
+)
